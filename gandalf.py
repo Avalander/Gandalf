9593d1e9d57d020b882eda357451fb3397b2beb5
Avalander <darth.quesorico@gmail.com>
2017-03-19 08:37:12 +0100

    Applied pep8 style

--- gandalf.py
+++ gandalf.py
@@ -12,6 +12,7 @@ commands = [
 
 _gandalf_dir = os.path.join(os.path.expanduser('~'), '.gandalf')
 
+
 def create(name):
 	gandalf_dir = _gandalf_dir
 	script_file = os.path.join(gandalf_dir, name)
@@ -22,6 +23,7 @@ def create(name):
 	open(script_file, 'a').close()
 	return_code = subprocess.call(['nano', script_file])
 
+
 def run(name):
 	script_file = os.path.join(os.path.expanduser('~'), '.gandalf', name)
 	if not os.path.exists(script_file):
@@ -30,8 +32,10 @@ def run(name):
 	result = subprocess.check_call(['/bin/bash', script_file])
 	print('\n* Execution finished *\n')
 
+
 def _list(name):
 	print(os.listdir(_gandalf_dir))
+
 
 def remove(name):
 	script_file = os.path.join(_gandalf_dir, name)
@@ -41,6 +45,7 @@ def remove(name):
 		if os.path.exists(script_file):
 			print('Could not remove script \'{}\''.format(script_file))
 
+
 command_handlers = {
 	'create': create,
 	'list': _list,
@@ -48,12 +53,14 @@ command_handlers = {
 	'remove': remove
 }
 
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('command', metavar='C', help='Command to execute', choices=commands)
 	parser.add_argument('name', help='Name of the script to create')
 	args = parser.parse_args()
 	command_handlers[args.command](args.name)
+
 
 if __name__ == '__main__':
 	main()
